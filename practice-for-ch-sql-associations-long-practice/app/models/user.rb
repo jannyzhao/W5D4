95639342b1781c9aa3b7e1bf83951869558f6a2b
Janny Zhao <zhao.janny@gmail.com>
2023-01-05 15:27:49 -0800

    Initial commit

---
+++ practice-for-ch-sql-associations-long-practice/app/models/user.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+    # validates 
+    has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :student_id,
+        class_name: :Enrollment,
+        dependent: :destroy
+
+    has_many :enrolled_courses,
+        through: :enrollments,
+        source: :course,
+        dependent: :destroy
+
+     has_many :courses,
+        primary_key: :id,
+        foreign_key: :instructor_id,
+        class_name: :Course,
+        dependent: :destroy
+end
